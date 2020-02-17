3271c4a27165f2fa858be6188d4df9286b9414c0
wolfpat01 <wolfpat01@users.noreply.github.com>
2020-02-17 13:23:27 +0100

    Update scketch.js

--- public/scketch.js
+++ public/scketch.js
@@ -92,13 +92,13 @@ function warfeilddata(data) {
 
 // setup
 function setup() {
-  socket = io.connect('http://sibiro.herokuapp.com/');
+  socket = io();
   this.connecttotheserver = function connetingtoserver() {
-    socket = io.connect('http://sibiro.herokuapp.com/');
+    socket = io();
   };
   // When press play in the html
   document.getElementById('play').onclick = function onclickplay() {
-    socket = io.connect('http://sibiro.herokuapp.com/');
+    socket = io();
     login();
   };
   login();
