4db7c5ef1d4d58355b67a2b56857c817b081914a
wolfpat01 <wolfpat01@users.noreply.github.com>
2020-02-21 19:10:43 +0100

    bloby physics update

--- application/public/objects/blob.js
+++ application/public/objects/blob.js
@@ -24,12 +24,46 @@ class Blob {
     this.setrad = function setrad(r2) {
       this.r = r2;
     };
+    var timer =0;
+    var minX = -49980;
+    var maxX = 49980;
+    var minY = -49980;
+    var maxY = 49980;
     this.show = function showing() {
       if (this.x === null) { this.x = 0; }
       if (this.y === null) { this.y = 0; }
       // body
       fill(this.c1, this.c2, this.c3);
-      ellipse(this.x, this.y, this.r * 2, this.r * 2);
+      //ellipse(this.x, this.y, this.r * 2, this.r * 2);
+      var middotx = this.x;
+      var middoty = this.y;
+      beginShape();
+      timer += 50;
+      for(let i =0; i< TWO_PI;i += PI/40) {
+        var ofsetx = map(cos(i),-1,1,0,10);
+        var ofsety = map(sin(i),-1,1,0,10);
+        var r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/10 * 2) + this.r;
+        if((i <= PI/2 && (i >= 3 * PI / 2) && this.x < minX)) {
+          r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/100 * 2) + this.r;
+          console.log("minx");
+        }
+        if((i >= PI/2 && (i <= 3 * PI / 2) && this.x > maxX)) {
+          r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/100 * 2) + this.r;
+          console.log("maxx");
+        }
+        if((i <= 0 && (i >= PI) && this.y < minY)) {
+          r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/100 * 2) + this.r;
+          console.log("miny");
+        }
+        if((i >= 2*PI && (i <= PI) && this.y > maxY)) {
+          r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/100 * 2) + this.r;
+          console.log("maxy");
+        }
+        var dx = r * cos(i);
+        var dy = r * sin(i);
+        vertex(middotx + dx, middoty + dy);
+      }
+      endShape(CLOSE);
 
       // text
       fill(255);
