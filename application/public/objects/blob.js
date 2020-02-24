2eba959561e23aa53635e1075bcbac8d850cf2fd
wolfpat01 <wolfpat01@users.noreply.github.com>
2020-02-24 18:32:29 +0100

    refactoring blob.js

--- application/public/objects/blob.js
+++ application/public/objects/blob.js
@@ -14,82 +14,56 @@ class Blob {
     this.c3 = c[2];
     this.vx = 0;
     this.vy = 0;
-    this.setcolor = function setcolor(c1, c2, c3) {
+    this.middot = createVector(1, 1);
+    this.vel = createVector(1, 1);
+  }
+    setcolor(c1, c2, c3) {
       this.c1 = c1;
       this.c2 = c2;
       this.c3 = c3;
     };
-    this.middot = createVector(1, 1);
-    this.vel = createVector(1, 1);
-    this.setrad = function setrad(r2) {
+
+    setrad(r2) {
       this.r = r2;
     };
-    var timer =0;
-    var minX = -49980;
-    var maxX = 49980;
-    var minY = -49980;
-    var maxY = 49980;
-    this.preload = function preload(){
-      br = loadFont('fonts/br.ttf');
+
+    preload(){
+      //br = loadFont('fonts/br.ttf');
     }
-    this.show = function showing(br) {
-      if (this.x === null) { this.x = 0; }
-      if (this.y === null) { this.y = 0; }
-      // body
-      fill(this.c1, this.c2, this.c3);
-      //ellipse(this.x, this.y, this.r * 2, this.r * 2);
-      var middotx = this.x;
-      var middoty = this.y;
-      beginShape();
+    show(br) {
+      var timer =0;
+      if (this.x === null) { this.x = 0; } // if somehow x
+      if (this.y === null) { this.y = 0; } // or y = null it will equal 0
+      //// body
+      // fill blob with his color
+      fill(this.c1, this.c2, this.c3); 
+      // using 2d noise to create that bloby feels
+      beginShape(); // built in p5.js function 
       timer += 50;
       for(let i =0; i< TWO_PI;i += PI/40) {
         var ofsetx = map(cos(i),-1,1,0,10);
         var ofsety = map(sin(i),-1,1,0,10);
         var r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/10 * 0.7) + this.r;
-        /*if((i <= PI/2 && (i >= 3 * PI / 2) && this.x < minX)) {
-          r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/100 * 2) + this.r;
-          console.log("minx");
-        }
-        if((i >= PI/2 && (i <= 3 * PI / 2) && this.x > maxX)) {
-          r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/100 * 2) + this.r;
-          console.log("maxx");
-        }
-        if((i <= 0 && (i >= PI) && this.y < minY)) {
-          r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/100 * 2) + this.r;
-          console.log("miny");
-        }
-        if((i >= 2*PI && (i <= PI) && this.y > maxY)) {
-          r = map(noise(ofsetx/2  + this.x/this.r ,ofsety/2+ this.y/this.r , timer/this.r),0,1,0,this.r/100 * 2) + this.r;
-          console.log("maxy");
-        }*/
         var dx = r * cos(i);
         var dy = r * sin(i);
-        vertex(middotx + dx, middoty + dy);
+        vertex(this.x + dx, this.y + dy);
       }
-      endShape(CLOSE);
+      endShape(CLOSE); // ends of beginShape();
 
-      // text
-
-      textFont(br);
-      fill(255);
-      stroke(8);
-      textSize(0.3 * this.r);
-      textAlign(CENTER);
+      //// text
+      textFont(br); // use font br on the files
+      fill(255); // fill with white
+      stroke(8); 
+      textSize(0.3 * this.r); // the size of the text
+      textAlign(CENTER); // the alignment
+      // showing the weight text of the blob
       text([parseInt(this.r)], this.x - 2, this.y + [parseInt(this.r)] / 2);
-      fill(255,0,0);
+      fill(255,0,0); // fill color
       stroke(0);
-      strokeWeight(10)
+      strokeWeight(10);
       textSize(0.3 * this.r);
       textAlign(CENTER);
+      // show the nickname of the blob/player
       text(this.nickname, this.x - 2, this.y);
-    };
-    this.update = function updating() {
-      // calculating mouse possition
-      const vel = createVector(mouseX, mouseY);
-      vel.sub((width / 2) - this.middot.x, (height / 2) - this.middot.y);
-      vel.setMag(3);
-      this.vx = vel.x;
-      this.vy = vel.y;
-    };
-  }
+    };  
 }
--- application/public/objects/player.js
+++ application/public/objects/player.js
@@ -9,34 +9,12 @@ class Player {
     this.isitshown = false;
     this.blobs = [];
   }
-
-  update() {
-    if (this.isitshown) {
-      this.blobs.forEach(blob => {
-        blob.update();
-        blob.middot = this.middot;
-      });
-    }
-  };
+  // show blobs
   show(br) {
     if (this.isitshown) {
       this.blobs.forEach(blob => {
         blob.show(br);
       });
     }
-  };
-  updatetext() {
-    socket.on('someonechangedhisname', someonechangedhisname);
-    fill(120);
-    textAlign(CENTER);
-    textSize(16);
-    text(this.nickname, this.pos.x, this.pos.y, this.r);
-  };
-  updatepos(newx, newy) {
-    this.pos.x = newx;
-    this.pos.y = newy;
-  };
-  setrad(r) {
-    this.r = r;
   };
 }
